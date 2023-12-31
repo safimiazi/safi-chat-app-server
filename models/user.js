b6176f6990d0fed4fb2af15c3c514b0e390b4a77
safimiazi <mohibullamiazi@gmail.com>
2023-12-31 23:33:03 +0600

    validator added

--- models/user.js
+++ models/user.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         require: [true, "Email is required"],
+        validate: {
+            validator: function name(params) {
+                
+            }
+        }
 
     }
 })
