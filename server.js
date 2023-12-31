e43814925200e025d1c4f4c88769af1058ac8dae
safimiazi <mohibullamiazi@gmail.com>
2023-12-31 20:29:26 +0600

    mongoose connect to the db

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
+dotenv.config({ path: "./config.env" })
+process.on("uncaughtException", (err) => {
+    console.log(err);
+    process.exit(1);
+})
+
 const http = require("http");
 const server = http.createServer(app);
 
+const DB = process.env.DB_URI.replace("<PASSWORD>", process.env.DB_PASSWORD)
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then((con) => {
+        console.log("DB connection is successful");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+
+
 const port = process.env.PORT || 8000;
 
-server.listen(port, ()=> {
+server.listen(port, () => {
     console.log(`app running on port ${port}`);
+})
+
+process.on("unhandledRejection", (err) => {
+    console.log(err);
+    server.close(() => {
+        process.exit(1);
+    });
 })
