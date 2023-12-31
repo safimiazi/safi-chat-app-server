26e71820f1f64154a2e31b57dcf53f013876cf51
safimiazi <mohibullamiazi@gmail.com>
2023-12-31 18:09:43 +0600

    cors install

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize")
 const bodyParser = require("body-parser");
-
-
 const xss = require("xss");
-
 const app = express()
-
+const cors = require("cors")
 app.use(express.json({limit: "10kb"}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet())
-
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "PATCH", "POST", "DELETE", "PUT"],
+    credentials: true,
+}))
 if(process.env.NODE_ENV === "development"){
     app.use(morgan("dev"));
 }
