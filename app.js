cc63ea14a3781b58ec2f410302bec525e66310a5
safimiazi <mohibullamiazi@gmail.com>
2023-12-31 17:00:21 +0600

    comment added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express"); // web frameWork for node.js
-const morgan = require("morgan")
+const morgan = require("morgan")  //http request logger middleware for node.js
 const app = express()
 
 module.exports = app;
