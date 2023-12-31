0e2dba0e315d5ab19749246be2379ab1d5c991cf
safimiazi <mohibullamiazi@gmail.com>
2023-12-31 16:45:01 +0600

    added port in the server.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const app = require("./app");
 const http = require("http");
 const server = http.createServer(app);
+
+const port = process.env.PORT
+
+server.listen(port, ()=> {
+    console.log(`app running on port ${port}`);
+})
