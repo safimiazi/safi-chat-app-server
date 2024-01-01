896723922469c234712991e29e759d6f11b310d4
safimiazi <mohibullamiazi@gmail.com>
2024-01-01 15:38:36 +0600

    added if condition

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,4 +3,11 @@ const User = require("../models/user")
 
 exports.login = async(req, res, next) => {
     const {email, password} = req.body;
+
+    if(!email || !password){
+        res.status(400).json({
+            status: "error",
+            message: "Both email and password are required"
+        })
+    }
 }
