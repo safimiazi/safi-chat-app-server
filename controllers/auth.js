fcd7f97bc2141dafe73c076f7646e06fc44a3f7f
safimiazi <mohibullamiazi@gmail.com>
2024-01-01 15:56:08 +0600

    user find

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,24 @@
 const jwt = require('jsonwebtoken');
 const User = require("../models/user")
 
-exports.login = async(req, res, next) => {
-    const {email, password} = req.body;
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
 
-    if(!email || !password){
+    if (!email || !password) {
         res.status(400).json({
             status: "error",
             message: "Both email and password are required"
         })
     }
+
+const user = await User.findOne({email: email}).select("+password");
+
+if(!user){
+    res.status(400).json({
+        status: "error",
+        message: "Email is Incorrect"
+    })
+}
+
+
 }
