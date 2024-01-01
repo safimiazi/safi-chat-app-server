e8467342a2f41f40ae436a33bddffccc627ebbd6
safimiazi <mohibullamiazi@gmail.com>
2024-01-01 18:46:49 +0600

    create new user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,37 @@
 const jwt = require('jsonwebtoken');
-const User = require("../models/user")
+const User = require("../models/user");
+const filterObj = require('../utils/filterObj');
 
-const signToken = (userId) => jwt.sign({userId}, process.env.JWT_SECRET);
+const signToken = (userId) => jwt.sign({ userId }, process.env.JWT_SECRET);
 
 
 //register new user:
-exports.register = async(req, res, next) => {
-    const {firstName, lastName, email, password} = req.body;
+exports.register = async (req, res, next) => {
+    const { firstName, lastName, email, password } = req.body;
 
+
+    const filteredBody = filterObj(req.body, "firstName", "lastName", "password", "email");
     //check if a verified user with given email exists
-    const existing_user = await User.findOne({email: email});
+    const existing_user = await User.findOne({ email: email });
 
-    if(existing_user && existing_user.verified){
+    if (existing_user && existing_user.verified) {
         res.status(400).json({
             status: "error",
             message: "Email is already in use, Please login"
         })
-    }else if(existing_user) {
-        await User.findOneAndUpdate({email: email}, {...req.body},)
-    }else{
-        
+    } else if (existing_user) {
+        await User.findOneAndUpdate({ email: email }, filteredBody, { new: true, validateModifiedOnly: true })
+        //generate OTP and send email to user
+        req.userId = existing_user._id;
+        next();
+    } else {
+        //if user record is not available in DB
+        const new_user = await User.create(filteredBody)
+
+        //generate OTP and send email to user
+        req.userId = new_user._id
+        next();
+
     }
 }
 
@@ -46,7 +58,7 @@ exports.login = async (req, res, next) => {
     const token = signToken(userDoc._id);
 
     res.status(200).json({
-        status: "success", 
+        status: "success",
         message: "Logged in successfully",
         token,
     })
