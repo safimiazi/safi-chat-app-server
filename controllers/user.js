84d9842001b6f3ba4afe1721e82db621688f6ef0
safimiazi <mohibullamiazi@gmail.com>
2024-01-16 12:54:37 +0600

    feat: implement getRequests endpoint
    Add endpoint to retrieve friend requests for the logged-in user.
    Uses FriendRequest model to find requests and populates sender details.
    Response includes status, data (requests), and a success message.1~

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const FriendRequest = require("../models/friendRequest");
 const User = require("../models/user");
 const filterObj = require("../utils/filterObj");
 
@@ -30,5 +31,30 @@ exports.getUsers = async (req, res, next) => {
         status: "success",
         data: remaining_users,
         message: "Users found successfully",
+    })
+};
+
+
+exports.getRequests = async (req, res, next) => {
+    const requests = await FriendRequest.find({
+        recipient: req.user._id,
+    }).populate("sender", "_id firstName lastName");
+
+    res.status(200).json({
+        status: "success",
+        data: requests,
+        message: "Friends requests Found Successfully"
+    })
+}
+
+
+
+exports.getFriends = async (req, res, next) => {
+    const friends = await User.findById(req.user._id).populate("friends", "_id firstName lastName")
+
+    res.status(200).json({
+        status: "success",
+        data: friends,
+        message: "Friends Found Successfully"
     })
 }
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,7 @@ const authController = require("../controllers/auth")
 const userController = require("../controllers/user")
 
 router.post("/update-me",authController.protect, userController.updateMe);
-router.post("/get-users", authController.protect, userController.getUsers);
+router.get("/get-users", authController.protect, userController.getUsers);
+router.get("/get-friends", authController.protect, userController.getFriends);
+router.get("/get-friend-requests", authController.protect, userController.getRequests);
 module.exports = router;
